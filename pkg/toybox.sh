01880b0416c4ea63771cc2a3484c67c78855662a
Eivind Uggedal <eivind@uggedal.com>
2015-01-28 09:42:12 +0000

    pkg: toybox

---
+++ pkg/toybox.sh
@@ -0,0 +1,14 @@
+ver=0.5.1
+rev=1
+src=http://landley.net/$name/downloads/$name-${ver}.tar.bz2
+
+style=make
+
+pre_build() {
+  # TODO: save config and maybe stip it
+  make defconfig
+}
+
+do_install() {
+  PREFIX=$DESTDIR make install
+}
