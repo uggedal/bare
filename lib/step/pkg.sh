3c77b749d56623b4b92e0b0def62aa6c8aa72253
Eivind Uggedal <eivind@uggedal.com>
2015-01-29 22:34:11 +0000

    pkg: print size stats

--- lib/step/pkg.sh
+++ lib/step/pkg.sh
@@ -1,8 +1,17 @@
+_xz_stat() {
+  printf -- '%s' "$1" | awk "{ print \$$2 \" \" \$$3 }"
+}
+
 step_pkg() {
   local pkgdest=$_DEST/$fullname
   local pkg=$_REPO/${fullname}.tar.xz
+  local stat
 
   progress pkg "'$name'"
 
   tar -C $pkgdest -cJvf $pkg .
+
+  stat="$(xz -l $pkg | tail -n1)"
+  msg "Uncompressed: $(_xz_stat "$stat" 5 6)"
+  msg "Compressed:   $(_xz_stat "$stat" 3 4)"
 }
