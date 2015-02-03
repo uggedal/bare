16a33531dacc0504f5ba14634d938b9b16419f52
Eivind Uggedal <eivind@uggedal.com>
2015-02-03 15:05:52 +0000

    env: set host/build triplet

--- lib/env.sh
+++ lib/env.sh
@@ -1,6 +1,9 @@
 umask 022
 export LC_ALL=C
 
+MK_ARCH=$(uname -m)
+MK_TRIPLET=$MK_ARCH-linux-musl
+
 [ "$MK_NPROC" ] ||
   MK_NPROC=$(printf -- '%s\n' /sys/devices/system/cpu/cpu[0-9]* | wc -l)
 
@@ -14,4 +17,6 @@ MK_CONFIGURE="
   --sysconfdir=/etc
   --localstatedir=/var
   --mandir=/usr/share/man
+  --host=$MK_TRIPLET
+  --build=$MK_TRIPLET
   "
