ec4c8602898ae948c4578372bbd5522edbd1cc1b
Eivind Uggedal <eivind@uggedal.com>
2015-02-03 11:02:00 +0000

    pkg: busybox

---
+++ pkg/busybox.sh
@@ -0,0 +1,13 @@
+ver=1.23.1
+rev=1
+src=http://${name}.net/downloads/$name-${ver}.tar.bz2
+
+style=make
+
+pre_build() {
+  make allnoconfig KCONFIG_ALLCONFIG=$MK_FILE/config
+}
+
+do_install() {
+  make CONFIG_PREFIX=$MK_DESTDIR install
+}
