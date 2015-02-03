ca84a50602017acab24737176588d0b01845edf1
Eivind Uggedal <eivind@uggedal.com>
2015-02-03 23:19:40 +0000

    gcc: sort configure

--- pkg/gcc.sh
+++ pkg/gcc.sh
@@ -4,15 +4,15 @@ src=$URI_GNU/$name/$name-${ver}/$name-${ver}.tar.bz2
 
 style=configure
 configure='
-  --with-newlib
   --disable-nls
-  --disable-shared
-  --disable-multilib
   --disable-decimal-float
   --disable-libgomp
   --disable-libmudflap
   --disable-libquadmath
   --disable-libssp
+  --disable-multilib
+  --disable-shared
   --disable-threads
   --enable-languages=c
+  --with-newlib
   '
