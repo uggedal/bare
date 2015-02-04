0841989aa8cbcd9d8900c42871b2598401730196
Eivind Uggedal <eivind@uggedal.com>
2015-02-04 16:53:57 +0000

    bootstrap-gcc: tweak configure

--- pkg/bootstrap-gcc.sh
+++ pkg/bootstrap-gcc.sh
@@ -2,16 +2,10 @@ inherit gcc
 
 configure='
   --disable-libmudflap
-  --disable-libquadmath
-  --disable-libssp
-  --disable-libstdcxx-pch
+  --disable-libsanitizer
   --disable-multilib
   --disable-nls
-  --enable-c99
   --enable-languages=c,c++,lto
-  --enable-libstdcxx-time
-  --enable-long-long
   --enable-lto
   --enable-shared
-  --enable-threads=posix
   '
