e694ac2f229408a2e974f177340d5ff2ad88bcc3
Eivind Uggedal <eivind@uggedal.com>
2015-02-04 16:33:22 +0000

    pkg: bootstrap-linux-headers

--- pkg/linux.sh
+++ pkg/linux.sh
@@ -1,24 +1,9 @@
 ver=3.14.31
 rev=1
-src=https://www.kernel.org/pub/$name/kernel/v3.x/$name-${ver}.tar.xz
+src=https://www.kernel.org/pub/$parentname/kernel/v3.x/$parentname-${ver}.tar.xz
 
 if [ -z "$MK_BOOTSTRAP" ]; then
   bdep='perl'
 fi
 
 style=make
-
-do_build() {
-  make ARCH=$MK_KERNEL_ARCH headers_check
-}
-
-do_install() {
-  make \
-    ARCH=$MK_KERNEL_ARCH \
-    INSTALL_HDR_PATH=$MK_DESTDIR/$MK_PREFIX/$MK_TRIPLET \
-    headers_install
-}
-
-post_install() {
-  find $MK_DESTDIR -name .install -o -name ..install.cmd | xargs rm -f
-}
