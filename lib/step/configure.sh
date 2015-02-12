fe6292b8b4cb752aed0e6f897f51e7d1da276d01
Eivind Uggedal <eivind@uggedal.com>
2015-02-12 14:57:23 +0000

    configure: subshell to preserve cwd

--- lib/step/configure.sh
+++ lib/step/configure.sh
@@ -1,8 +1,8 @@
 step_configure() {
   progress configure "'$name' using $style"
 
-  local ocwd=$(pwd)
-  cd $MK_BUILD
-  run_style $style configure
-  cd $ocwd
+  (
+    cd $MK_BUILD
+    run_style $style configure
+  )
 }
