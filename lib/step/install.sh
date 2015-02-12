64654c7deb51a742738bd5f43569f07e22ec037c
Eivind Uggedal <eivind@uggedal.com>
2015-02-12 15:01:13 +0000

    install: subshell to preserve cwd

--- lib/step/install.sh
+++ lib/step/install.sh
@@ -1,9 +1,10 @@
 step_install() {
   progress install "'$name' using $style"
 
-  local ocwd=$(pwd)
-  cd $MK_BUILD
   mkdir -p $MK_DESTDIR
-  run_style $style install
-  cd $ocwd
+
+  (
+    cd $MK_BUILD
+    run_style $style install
+  )
 }
