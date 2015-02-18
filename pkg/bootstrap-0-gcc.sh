0f4ef701392f577c19d461d0756d218bb35f0033
Eivind Uggedal <eivind@uggedal.com>
2015-02-18 14:57:03 +0000

    pkg: gcc

---
+++ pkg/bootstrap-0-gcc.sh
@@ -0,0 +1,19 @@
+inherit gcc
+
+CFLAGS='-O0 -g0'
+CXXFLAGS="$CFLAGS"
+
+configure='
+  --disable-decimal-float
+  --disable-libatomic
+  --disable-libgomp
+  --disable-libmudflap
+  --disable-libquadmath
+  --disable-libssp
+  --disable-multilib
+  --disable-nls
+  --disable-shared
+  --disable-threads
+  --enable-languages=c
+  --with-newlib
+  '
---
+++ pkg/gcc.sh
@@ -0,0 +1,19 @@
+ver=4.9.2
+rev=1
+src=$URI_GNU/$name/$name-${ver}/$name-${ver}.tar.bz2
+bdep='zlib-dev'
+
+style=configure
+
+configure='
+  --disable-multilib
+  --disable-nls
+  --enable-checking=release
+  --enable-languages=c,c++,lto
+  --enable-lto
+  --enable-shared
+  --enable-threads=posix
+  --with-system-zlib
+  '
+
+builddir=gcc-build
