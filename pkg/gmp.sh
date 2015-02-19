2c3d28d3d0abede9ec8978ac40603d35ce6c3d6b
Eivind Uggedal <eivind@uggedal.com>
2015-02-19 15:45:04 +0000

    gmp: fix version

--- pkg/gmp.sh
+++ pkg/gmp.sh
@@ -1,6 +1,6 @@
-ver=6.0.0a
+ver=6.0.0
 rev=1
-src=https://gmplib.org/download/$name/${name}-${ver}.tar.xz
+src=https://gmplib.org/download/$name/${name}-${ver}a.tar.xz
 bdep='zlib-dev'
 
 style=configure
