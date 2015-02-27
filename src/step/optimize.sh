a70b72bba9d7156f3c91929cdbfa8ef01156a290
Eivind Uggedal <eivind@uggedal.com>
2015-02-27 14:36:43 +0000

    optimize: strip

--- src/step/optimize.sh
+++ src/step/optimize.sh
@@ -2,5 +2,21 @@ step_optimize() {
   progress optmimize "'$name'"
 
   # Remove libtool archives
-  find $_DEST/$fullname -type f -name \*.la -delete
+  find $_DEST/$fullname -type f -name \*.la -exec rm -v {} \+
+
+  # Strip binaries
+  local f
+  find $_DEST/$fullname -type f | while read f; do
+    case "$(file -bi "$f")" in
+      application/x-executable*)
+        ${STRIP:-strip} -v $f
+        ;;
+      application/x-sharedlib*)
+        ${STRIP:-strip} -v --strip-unneeded $f
+        ;;
+      application/x-archive*)
+        ${STRIP:-strip} -v --strip-debug $f
+        ;;
+    esac
+  done
 }
