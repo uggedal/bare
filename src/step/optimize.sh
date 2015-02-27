aad37661fb151a5bf06a96a2a8cfa2ff0d38f96b
Eivind Uggedal <eivind@uggedal.com>
2015-02-27 20:48:21 +0000

    optimize: status

--- src/step/optimize.sh
+++ src/step/optimize.sh
@@ -1,22 +1,58 @@
-step_optimize() {
-  progress optmimize "'$name'"
+_rootify() {
+  printf -- '%s' ${1#$_DEST/$fullname}
+}
 
-  # Remove libtool archives
-  find $_DEST/$fullname -type f -name \*.la -exec rm -v {} \+
-
-  # Strip binaries
+remove_libtool_archives() {
   local f
+
+  find $_DEST/$fullname -type f -name \*.la | while read f; do
+    msg "remove libtool archive: $(_rootify $f)"
+    rm $f
+  done
+}
+
+remve_info_pages() {
+  local f
+
+  find $_DEST/$fullname$MK_PREFIX/share/info -type f | while read f; do
+    msg "remove info page: $(_rootify $f)"
+    rm $f
+  done
+}
+
+strip_binaries() {
+  local f type args
+
   find $_DEST/$fullname -type f | while read f; do
+    type=
+    args=
     case "$(file -bi "$f")" in
       application/x-executable*)
-        ${STRIP:-strip} -v $f
+        type=executable
         ;;
       application/x-sharedlib*)
-        ${STRIP:-strip} -v --strip-unneeded $f
+        type='shared lib'
+        args=--strip-unneeded
         ;;
       application/x-archive*)
-        ${STRIP:-strip} -v --strip-debug $f
+        type='static lib'
+        args=--strip-debug
         ;;
     esac
+
+    if [ "$type" ]; then
+      msg "strip $type: $(_rootify $f)"
+      ${STRIP:-strip} $args $f
+    fi
   done
+}
+
+step_optimize() {
+  local f
+
+  progress optmimize "'$name'"
+
+  remove_libtool_archives
+  remve_info_pages
+  strip_binaries
 }
