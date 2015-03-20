b3b4099332cd1485ef87f7691c6393ad1b86ea7b
Eivind Uggedal <eivind@uggedal.com>
2015-03-20 12:27:22 +0000

    pkg: helpers

--- src/lib/pkg.sh
+++ src/lib/pkg.sh
@@ -1,3 +1,22 @@
+pkg_to_fullname() {
+  local pkg=$1
+
+  printf -- '%s' ${pkg%$PKG_EXT*}
+}
+
+pkg_to_name() {
+  local pkg=$1
+
+  printf -- '%s' ${pkg%-*}
+}
+
+pkg_to_version() {
+  local pkg=$1
+  local fullname=$(pkg_to_fullname $pkg)
+
+  printf -- '%s' ${fullname##*-}
+}
+
 read_pkg() {
   local _v
   for _v in $PKG_VARS; do
