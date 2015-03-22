d599eac7ed4a378fdab5a9a0ebcfe1ffb98866ec
Eivind Uggedal <eivind@uggedal.com>
2015-03-22 13:14:57 +0000

    pkg: makefile

--- pkg/pkg.sh
+++ pkg/pkg.sh
@@ -1,17 +1,9 @@
 ver=0.0.1
 rev=1
 
-style=noop
+style=make
 
 do_extract() {
   mkdir -p $MK_SRC
-  cp -a $_SRC/pkg $_SRC/common $MK_SRC
-}
-
-do_build() {
-  $CC $CFLAGS -Wall pkg/pkg-contain.c -o pkg-contain
-}
-
-do_install() {
-  install -Dm755 pkg-contain $MK_DESTDIR$MK_PREFIX/bin/pkg-contain
+  cp -a $_SRC/Makefile $_SRC/common $_SRC/pkg $MK_SRC
 }
