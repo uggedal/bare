b7f681c6fa43106975e60c41c40fe34ecffadde9
Eivind Uggedal <eivind@uggedal.com>
2015-03-27 09:48:16 +0000

    pkg: linux-headers

--- pkg/bootstrap-linux-headers.sh
+++ pkg/bootstrap-linux-headers.sh
@@ -1,16 +1,1 @@
-inherit linux
-
-do_build() {
-  make ARCH=$MK_KERNEL_ARCH headers_check
-}
-
-do_install() {
-  make \
-    ARCH=$MK_KERNEL_ARCH \
-    INSTALL_HDR_PATH=$MK_DESTDIR/$MK_PREFIX \
-    headers_install
-}
-
-post_install() {
-  find $MK_DESTDIR -name .install -o -name ..install.cmd | xargs rm -f
-}
+inherit linux-headers
