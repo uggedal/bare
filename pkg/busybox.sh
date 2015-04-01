bd6871f8b0cdc2e4b1ac9ded5ee9c128270f9422
Eivind Uggedal <eivind@uggedal.com>
2015-04-01 08:16:22 +0000

    busybox: bdep linux-headers

--- pkg/busybox.sh
+++ pkg/busybox.sh
@@ -4,6 +4,8 @@ dist \
   http://${PKG_NAME}.net/downloads/$PKG_NAME-${PKG_VER}.tar.bz2 \
   http://git.alpinelinux.org/cgit/aports/plain/main/busybox/0001-linedit-deluser-use-POSIX-getpwent-instead-of-getpwe.patch
 
+bdep linux-headers
+
 pre_build() {
   make allnoconfig KCONFIG_ALLCONFIG=$MK_FILE/config
 }
