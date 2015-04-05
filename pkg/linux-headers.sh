d58ee57833ef5db53adb7163d723a99817ad7607
Eivind Uggedal <eivind@uggedal.com>
2015-04-05 19:31:54 +0000

    bootstrap: install linux-headers

--- pkg/linux-headers.sh
+++ pkg/linux-headers.sh
@@ -7,7 +7,7 @@ do_build() {
 do_install() {
   make \
     ARCH=$MK_KERNEL_ARCH \
-    INSTALL_HDR_PATH=$MK_DESTDIR/$MK_PREFIX \
+    INSTALL_HDR_PATH=$MK_DESTDIR$MK_PREFIX \
     headers_install
 }
 
