a206bfe77bc5b0b93ae740df441d6964e8b0149d
Eivind Uggedal <eivind@uggedal.com>
2015-04-13 19:13:14 +0000

    pkg-create: support storing lib and dep

--- src/pkg/pkg-create.sh
+++ src/pkg/pkg-create.sh
@@ -4,9 +4,10 @@
 . @@LIBDIR@@/pkg/pkg.sh
 . @@LIBDIR@@/pkg/msg.sh
 
-_USAGE='-n name -r root -p pkg'
+_USAGE='-n name -r root -p pkg
+[-l libs] [-d deps]'
 
-while getopts "n:r:p" opt; do
+while getopts "n:r:p:l:d:" opt; do
   case $opt in
     n)
       FULLNAME=$OPTARG
@@ -16,6 +17,12 @@ while getopts "n:r:p" opt; do
       ;;
     p)
       PKG=$OPTARG
+      ;;
+    l)
+      LIBS="$OPTARG"
+      ;;
+    d)
+      DEPS="$OPTARG"
       ;;
   esac
 done
@@ -38,6 +45,14 @@ trap "rm -f $TMP" INT TERM EXIT
   [ "$1" != \* ] || die "no files in '$ROOT'"
 
   printf -- 'v|%s\n' $VERSION > $TMP
+
+  for lib in $LIBS; do
+    printf -- 'l|%s\n' $lib
+  done >> $TMP
+
+  for dep in $DEPS; do
+    printf -- 'd|%s\n' $dep
+  done >> $TMP
 
   find * -type f | sort | while read f; do
     printf -- 'f|%s|%s\n' $f $(file_sum $f)
