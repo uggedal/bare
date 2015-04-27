a28ddd10280412ada136f6261e9521cecc9a8e3e
Eivind Uggedal <eivind@uggedal.com>
2015-04-27 14:59:59 +0000

    pkg: ubase

---
+++ pkg/ubase.sh
@@ -0,0 +1,28 @@
+ver 0.0.001.d988f0
+rev 1
+dist http://git.suckless.org/$PKG_NAME/snapshot/$PKG_NAME-${PKG_VER##*.}.tar.bz2
+
+distdir $PKG_NAME-${PKG_VER##*.}
+
+pre_configure() {
+  ed config.mk <<EOF
+,s|^\(CC = \).*\$|\1$CC
+,s|^\(PREFIX = \).*\$|\1$MK_PREFIX
+/^CFLAGS/s|=|+=
+w
+q
+EOF
+}
+
+post_install() {
+  local unneeded='
+    eject
+    fsfreeze
+  '
+
+  local f
+  for f in $unneeded; do
+    rm $MK_DESTDIR$MK_PREFIX/bin/$f
+    rm -f $MK_DESTDIR$MK_PREFIX/share/man/man[18]/${f}.[18]
+  done
+}
