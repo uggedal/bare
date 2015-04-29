8d0e3bd12ac10b17e73d3cb829324a46deebf7b6
Eivind Uggedal <eivind@uggedal.com>
2015-04-29 14:53:47 +0000

    pkg: bzip2

---
+++ pkg/bzip2.sh
@@ -0,0 +1,28 @@
+ver 1.0.6
+rev 1
+dist http://bzip.org/$PKG_VER/$PKG_NAME-${PKG_VER}.tar.gz
+
+pre_configure() {
+  ed Makefile <<EOF
+,s/^\(CC\)=\(.*\)\$/\1\?=\2
+,s/^\(AR\)=\(.*\)\$/\1\?=\2
+,s/^\(RANLIB\)=\(.*\)\$/\1\?=\2
+w
+q
+EOF
+}
+
+do_build() {
+  make -j $MK_NPROC bzip2
+}
+
+do_install() {
+  install -Dm755 bzip2 $MK_DESTDIR$MK_PREFIX/bin/bzip2
+  install -Dm644 bzip2.1 $MK_DESTDIR$MK_MANDIR/man1/bzip2.1
+
+  local l
+  for l in bunzip2 bzcat; do
+    ln -s bzip2 $MK_DESTDIR$MK_PREFIX/bin/$l
+    ln -s bzip2.1 $MK_DESTDIR$MK_MANDIR/man1/${l}.1
+  done
+}
