6205a5ddb56212952f88c63a03ab44c66eedbb47
Eivind Uggedal <eivind@uggedal.com>
2015-05-08 10:32:06 +0000

    pkg: add needed libs

--- src/mk/step_pkg.sh
+++ src/mk/step_pkg.sh
@@ -18,6 +18,46 @@ _provided_libs() {
   done
 }
 
+_needed_libs() {
+  local f mime
+
+  find $_DEST/$PKG_QUALIFIED_NAME -type f | while read f; do
+    mime="$(file -bi "$f")"
+    case "$mime" in
+      application/x-executable*|application/x-sharedlib*)
+        objdump -p $f | awk '/^ +NEEDED/ { print $2 }'
+        ;;
+    esac
+  done
+}
+
+_find_pkg_with_lib() {
+  local name=$1
+  local ver=$2
+  local qualified_name=$3
+  local pkg=$4
+
+  [ $name != $PKG_NAME ] || return 0
+
+  local t f1 f2 l
+
+  while IFS='|' read -r t f1 f2; do
+    case $t in
+      l)
+        for l in $_NEEDED_LIBS; do
+          [ "$l" != "$f1" ] || printf '%s:%s\n' $name $l
+        done
+        ;;
+    esac
+  done < $_DB/$PKG_DB/$name
+}
+
+_lib_deps() {
+  _NEEDED_LIBS=$(_needed_libs)
+  read_repo $_REPO _find_pkg_with_lib | sort | uniq
+  unset _NEEDED_LIBS
+}
+
 _msg_list() {
   local prefix="$1"
   shift
@@ -37,7 +77,7 @@ step_pkg() {
   [ "$libs" ] || libs="$(_provided_libs)"
   _msg_list 'Provided lib:' $libs
 
-  local deps="$PKG_RDEP"
+  local deps="$PKG_RDEP $(_lib_deps)"
   _msg_list 'Run-time dep:' $deps
 
   pkg-create \
