31991a0dec490ecf24460461b61dbb1015a6e993
Eivind Uggedal <eivind@uggedal.com>
2015-05-10 09:10:12 +0000

    mpc: update to 1.0.3

--- pkg/mpc.sh
+++ pkg/mpc.sh
@@ -1,11 +1,7 @@
-ver 1.0.2
+ver 1.0.3
 rev 1
 dist $URI_GNU/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.gz
 bdep gmp-dev mpfr-dev
 
 configure \
   --with-pic
-
-pre_configure() {
-  cp -f $MK_FILE/config.sub .
-}
