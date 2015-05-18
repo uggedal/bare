4e204891d690e8313e99fc8c55b31ec50c105253
Eivind Uggedal <eivind@uggedal.com>
2015-05-18 13:27:20 +0000

    pkg-create: allow empty prefix

--- src/pkg/pkg-create.sh
+++ src/pkg/pkg-create.sh
@@ -41,8 +41,6 @@ trap "rm -f $TMP" INT TERM EXIT
 (
   cd $PREFIX
 
-  set -- *
-  [ "$1" != \* ] || die "no files in '$PREFIX'"
 
   printf 'v|%s\n' $VERSION > $TMP
 
@@ -54,17 +52,23 @@ trap "rm -f $TMP" INT TERM EXIT
     printf 'd|%s\n' $dep
   done >> $TMP
 
-  find * -type f | sort | while read f; do
-    printf 'f|%s|%s\n' $f $(file_sum $f)
-  done >> $TMP
+  set -- *
+  if [ "$1" = \* ]; then
+    err "no files in '$PREFIX'"
+    set --
+  else
+    find * -type f | sort | while read f; do
+      printf 'f|%s|%s\n' $f $(file_sum $f)
+    done >> $TMP
 
-  find * -type l | sort | while read l; do
-    printf '@|%s|%s\n' $l $(readlink $l)
-  done >> $TMP
+    find * -type l | sort | while read l; do
+      printf '@|%s|%s\n' $l $(readlink $l)
+    done >> $TMP
 
-  find * -type d | sort | while read d; do
-    printf '/|%s\n' $d
-  done >> $TMP
+    find * -type d | sort | while read d; do
+      printf '/|%s\n' $d
+    done >> $TMP
+  fi
 
   mkdir -p $PKG_DB
   mv $TMP $PKG_DB/$NAME
