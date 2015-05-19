89c8a5bee8bace0691abf08ceb6ba870378238f3
Eivind Uggedal <eivind@uggedal.com>
2015-05-19 18:26:00 +0000

    file: libmagic/libmagic-bld sub

--- pkg/file.sh
+++ pkg/file.sh
@@ -5,3 +5,8 @@ dist ftp://ftp.astron.com/pub/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.gz
 pre_configure() {
   cp -f $MK_FILE/config.sub .
 }
+
+sub libmagic type lib
+
+sub libmagic-bld type bld
+sub libmagic-bld rdep libmagic
