45c0166d3998d3a3d0d0552d98a7dea903a26523
Eivind Uggedal <eivind@uggedal.com>
2015-05-19 19:02:10 +0000

    binutils: binutils-bld

--- pkg/binutils.sh
+++ pkg/binutils.sh
@@ -2,6 +2,9 @@ ver 2.25
 rev 1
 dist $URI_GNU/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.bz2
 
+sub binutils-bld type bld
+sub binutils-bld rdep binutils
+
 configure \
   --disable-multilib \
   --disable-nls \
