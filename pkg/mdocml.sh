55399f90afbdf9c1fbf9a60bcac1ad460de9f11f
Eivind Uggedal <eivind@uggedal.com>
2015-05-20 16:18:06 +0000

    pkg: mdocml

---
+++ pkg/mdocml.sh
@@ -0,0 +1,28 @@
+ver 1.13.3
+rev 1
+dist http://${PKG_NAME}.bsd.lv/snapshots/$PKG_NAME-${PKG_VER}.tar.gz
+
+pre_configure() {
+  cat <<EOF >configure.local
+PREFIX=$MK_PREFIX
+MANDIR=$MK_MANDIR
+CFLAGS="\$CFLAGS $CFLAGS"
+BUILD_DB=0
+EOF
+}
+
+do_configure() {
+  ./configure
+}
+
+post_install() {
+  local d
+  for d in include lib share/man/man3; do
+    rm -r $MK_DESTDIR$MK_PREFIX/$d
+  done
+
+  rm $MK_DESTDIR$MK_PREFIX/bin/man
+  ln -s mandoc $MK_DESTDIR$MK_PREFIX/bin/man
+
+  install -Dm644 $MK_FILE/man.conf $MK_DESTDIR/etc/man.conf
+}
