552f126a8ad197788bce2d90baee3fd1059f6d69
Eivind Uggedal <eivind@uggedal.com>
2015-05-22 07:24:34 +0000

    def: mv PKG_ to mk

--- src/lib/def.sh
+++ src/lib/def.sh
@@ -1,31 +1,3 @@
-PKG_REQUIRED_VARS='
-  name
-  ver
-  rev
-  '
-
-PKG_OPTIONAL_VARS='
-  dist
-  bdep
-  rdep
-  lib
-  configure
-  distdir
-  builddir
-  emptydirs
-  '
-
-PKG_VARS="
-  $PKG_REQUIRED_VARS
-  $PKG_OPTIONAL_VARS
-  "
-
-PKG_COMPUTED_VARS='
-  qualified_name
-  parent_name
-  qualified_parent_name
-  '
-
 PKG_EXT=.tar.xz
 
 PKG_NAME_MAX=20
--- src/mk/def.sh
+++ src/mk/def.sh
@@ -12,6 +12,34 @@ MK_STEPS='
   pkg
 '
 
+PKG_REQUIRED_VARS='
+  name
+  ver
+  rev
+  '
+
+PKG_OPTIONAL_VARS='
+  dist
+  bdep
+  rdep
+  lib
+  configure
+  distdir
+  builddir
+  emptydirs
+  '
+
+PKG_VARS="
+  $PKG_REQUIRED_VARS
+  $PKG_OPTIONAL_VARS
+  "
+
+PKG_COMPUTED_VARS='
+  qualified_name
+  parent_name
+  qualified_parent_name
+  '
+
 PKG_SUB_VARS='
   type
   mv
