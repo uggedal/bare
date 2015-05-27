b7026b73160e49731420183b5e3da8665d16b135
Eivind Uggedal <eivind@uggedal.com>
2015-05-27 05:58:56 +0000

    file: func after decls

--- pkg/file.sh
+++ pkg/file.sh
@@ -2,11 +2,11 @@ ver 5.22
 rev 1
 dist ftp://ftp.astron.com/pub/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.gz
 
-pre_configure() {
-  cp -f $MK_FILE/config.sub .
-}
-
 sub libmagic type lib
 
 sub libmagic-bld type bld
 sub libmagic-bld rdep libmagic
+
+pre_configure() {
+  cp -f $MK_FILE/config.sub .
+}
