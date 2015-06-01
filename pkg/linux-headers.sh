f5cb87f566c2a6afcf49480dd5c9fa9584be5c49
Eivind Uggedal <eivind@uggedal.com>
2015-06-01 16:46:39 +0000

    linux: update to 4.0.4

--- pkg/linux-headers.sh
+++ pkg/linux-headers.sh
@@ -1,7 +1,5 @@
 inherit linux
 
-bdep ''
-
 do_build() {
   :
 }
--- pkg/linux.sh
+++ pkg/linux.sh
@@ -1,5 +1,3 @@
-ver 3.14.31
+ver 4.0.4
 rev 1
-dist https://www.kernel.org/pub/$PKG_NAME/kernel/v3.x/$PKG_NAME-${PKG_VER}.tar.xz
-
-bdep perl
+dist https://www.kernel.org/pub/$PKG_NAME/kernel/v${PKG_VER%%.*}.x/$PKG_NAME-${PKG_VER}.tar.xz
