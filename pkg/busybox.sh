7c8695caef4b0417dda353b383c7693fe8851583
Eivind Uggedal <eivind@uggedal.com>
2015-06-05 12:00:31 +0000

    busybox: cleanup

--- pkg/busybox.sh
+++ pkg/busybox.sh
@@ -1,10 +1,6 @@
 ver 1.23.2
 rev 1
-dist \
-  http://${PKG_NAME}.net/downloads/$PKG_NAME-${PKG_VER}.tar.bz2 \
-  http://git.alpinelinux.org/cgit/aports/plain/main/busybox/0001-linedit-deluser-use-POSIX-getpwent-instead-of-getpwe.patch
-
-bdep linux-headers
+dist http://${PKG_NAME}.net/downloads/$PKG_NAME-${PKG_VER}.tar.bz2
 
 pre_build() {
   make allnoconfig KCONFIG_ALLCONFIG=$MK_FILE/config
