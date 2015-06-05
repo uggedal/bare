ca581632a33e9c878ee87a411d2617360c2e01c9
Eivind Uggedal <eivind@uggedal.com>
2015-06-05 11:17:33 +0000

    bdepinstall: use shell for PATH lookup

--- src/mk/step_bdepinstall.sh
+++ src/mk/step_bdepinstall.sh
@@ -3,5 +3,5 @@ step_bdepinstall() {
 
   [ -z "$MK_NO_DEP" ] || return 0
 
-  [ -z "$PKG_BDEP" ] || contain pkg-install $PKG_BDEP
+  [ -z "$PKG_BDEP" ] || contain /bin/sh -lc pkg-install $PKG_BDEP
 }
