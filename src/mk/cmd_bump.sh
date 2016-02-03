364a07cf0caad2c2be5cde5560ea2597798946d6
Eivind Uggedal <eivind@uggedal.com>
2016-02-03 15:24:00 +0000

    bump: add patch/file changes

--- src/mk/cmd_bump.sh
+++ src/mk/cmd_bump.sh
@@ -1,8 +1,9 @@
 cmd_bump() {
   local v=$1
-  local f=$_PKG/$PKG_NAME.sh
+  local pkg=$_PKG/$PKG_NAME.sh
+  local f
 
-  ed $f <<EOF
+  ed $pkg <<EOF
 ,s|^\(ver \).*\$|\1$v
 w
 q
@@ -11,5 +12,11 @@ EOF
   ./mk fetch $PKG_NAME
   ./mk sum $PKG_NAME
   ./mk pkg $PKG_NAME
-  git ci -m "$PKG_NAME: update to $v" $f $_SUM/$PKG_NAME.sum
+
+  for f in $_SUM/$PKG_NAME.sum $_PATCH/$PKG_NAME $_FILE/$PKG_NAME $pkg; do
+	  if [ -e $f ]; then
+		  git add $f
+	  fi
+  done
+  git ci -m "$PKG_NAME: update to $v"
 }
