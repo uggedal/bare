ac43dcb1b8d29556364bc3770b9a3378f7252b2d
Eivind Uggedal <eivind@uggedal.com>
2016-02-06 22:28:14 +0000

    ksh: fix for gnu cp stupidity

--- pkg/ksh.sh
+++ pkg/ksh.sh
@@ -2,5 +2,6 @@ ver 5.7
 
 do_extract() {
 	mkdir -p $MK_DIST
-	cp -a $_SRC/ext/ksh/Makefile $_SRC/ext/ksh/dist/* $MK_DIST
+	cp -a $_SRC/ext/ksh/dist/* $MK_DIST
+	cp $_SRC/ext/ksh/Makefile $MK_DIST
 }
