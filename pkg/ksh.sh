75ac6b728d19873d65c9ecfb565ad1b2c40af042
Eivind Uggedal <eivind@uggedal.com>
2016-02-07 08:59:59 +0000

    ksh: generalize do_extract

--- pkg/ksh.sh
+++ pkg/ksh.sh
@@ -2,6 +2,6 @@ ver 5.7
 
 do_extract() {
 	mkdir -p $MK_DIST
-	cp -a $_SRC/ext/ksh/dist/* $MK_DIST
-	cp $_SRC/ext/ksh/Makefile $MK_DIST
+	cp -a $_SRC/ext/$PKG_NAME/dist/* $MK_DIST
+	cp $_SRC/ext/$PKG_NAME/Makefile $MK_DIST
 }
