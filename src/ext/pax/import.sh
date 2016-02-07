8a22bdfa12b67dda3c7774e863fe8d2b6675c812
Eivind Uggedal <eivind@uggedal.com>
2016-02-07 14:48:28 +0000

    pax: use libbsd

--- src/ext/pax/import.sh
+++ src/ext/pax/import.sh
@@ -4,12 +4,10 @@ RELEASE=5_7
 
 CVS=anoncvs@anoncvs.eu.openbsd.org
 PAX=src/bin/pax
-STDLIB=src/lib/libc/stdlib
-STRING=src/lib/libc/string
 INCLUDE=src/include
 GEN=src/lib/libc/gen
 
-for d in $PAX $STDLIB $STRING $INCLUDE $GEN; do
+for d in $PAX $INCLUDE $GEN; do
 	if ! [ -d $d ]; then
 		cvs -qd $CVS:/cvs get -rOPENBSD_$RELEASE -P $d
 	else
@@ -19,16 +17,11 @@ done
 
 mkdir -p dist
 
-cp $STDLIB/reallocarray.c dist/
-cp $STRING/strmode.c dist/
-
-for f in fts.h tzfile.h vis.h; do
+for f in fts.h tzfile.h; do
 	cp $INCLUDE/$f dist/
 done
 
-for f in fts.c vis.c; do
-	cp $GEN/$f dist/
-done
+cp $GEN/fts.c dist/
 
 cp -a $PAX/* dist/
 find dist -name CVS -type d | xargs rm -r
