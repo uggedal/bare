84d7afe2845e85cc9ddd60338928223cedfe9bff
Eivind Uggedal <eivind@uggedal.com>
2016-02-07 14:34:07 +0000

    ksh/import: remove leftover $SYS

--- src/ext/ksh/import.sh
+++ src/ext/ksh/import.sh
@@ -6,7 +6,7 @@ CVS=anoncvs@anoncvs.eu.openbsd.org
 KSH=src/bin/ksh
 GEN=src/lib/libc/gen
 
-for d in $KSH $GEN $SYS; do
+for d in $KSH $GEN; do
 	if ! [ -d $d ]; then
 		cvs -qd $CVS:/cvs get -rOPENBSD_$RELEASE -P $d
 	else
