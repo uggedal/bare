bd0e29a2cb29243764cded5b7cce134747628075
Eivind Uggedal <eivind@uggedal.com>
2016-02-09 19:58:38 +0000

    compress: new package

--- src/ext/compress/import.sh
+++ src/ext/compress/import.sh
@@ -4,14 +4,21 @@ RELEASE=5_8
 
 CVS=anoncvs@anoncvs.eu.openbsd.org
 COMPRESS=src/usr.bin/compress
+INCLUDE=src/include
+GEN=src/lib/libc/gen
 
-if ! [ -d $COMPRESS ]; then
-	cvs -qd $CVS:/cvs get -rOPENBSD_$RELEASE -P $COMPRESS
-else
-	cvs -qd $CVS:/cvs up -rOPENBSD_$RELEASE -P $COMPRESS
-fi
+for d in $COMPRESS $INCLUDE $GEN; do
+	if ! [ -d $d ]; then
+		cvs -qd $CVS:/cvs get -rOPENBSD_$RELEASE -P $d
+	else
+		cvs -qd $CVS:/cvs up -rOPENBSD_$RELEASE -P $d
+	fi
+done
 
 mkdir -p dist
+
+cp $INCLUDE/fts.h dist/
+cp $GEN/fts.c dist/
 
 cp -a $COMPRESS/* dist/
 find dist -name CVS -type d | xargs rm -r
