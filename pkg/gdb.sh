7e06d7583b64e4cfdf403e776db3f132a8b777a7
Eivind Uggedal <eivind@uggedal.com>
2016-02-10 17:03:21 +0000

    gdb: new package (compat patch from Alpine)

---
+++ pkg/gdb.sh
@@ -0,0 +1,22 @@
+ver 7.10.1
+dist $URI_GNU/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.xz
+
+bdep libz-bld linux-headers readline-bld
+
+configure \
+	--disable-gdbserver \
+	--disable-nls \
+	--with-system-readline \
+	--with-system-zlib
+
+pre_configure() {
+	ed configure <<EOF
+,s|MAKEINFO="\$MISSING makeinfo"|MAKEINFO=true|g
+w
+q
+EOF
+}
+
+post_install() {
+	rm -r $MK_DESTDIR$MK_PREFIX/include $MK_DESTDIR$MK_PREFIX/lib 
+}
