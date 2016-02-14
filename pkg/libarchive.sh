eef97db167283f9a9c700a880af4a705a0701174
Eivind Uggedal <eivind@uggedal.com>
2016-02-14 18:01:41 +0000

    libarchive: variour fixes

--- pkg/libarchive.sh
+++ pkg/libarchive.sh
@@ -3,14 +3,24 @@ dist http://www.libarchive.org/downloads/libarchive-${PKG_VER}.tar.gz
 
 configure \
 	--disable-acl \
+	--disable-bsdcpio \
 	--disable-posix-regex-lib \
 	--disable-xattr \
+	--enable-bsdtar=shared \
+	--without-bz2lib \
+	--without-iconv \
 	--without-lzma \
-	--without-xz \
+	--without-openssl \
 	--without-zlib
 
 sub libarchive-bld type bld
-sub libz-bld rdep libarchive
+sub libarchive-bld rdep libarchive
+sub libarchive-bld mv usr/share/man/man5
 
 sub bsdtar type custom
-sub bsdtar mv usr/bin/bsdtar usr/share/man/man1/bsdtar.5
+sub bsdtar mv usr/bin/bsdtar usr/share/man/man1/bsdtar.1
+sub bsdtar pkgorder after_main
+
+post_install() {
+	rm $MK_DESTDIR$MK_MANDIR/man1/cpio.1
+}
