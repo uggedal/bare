a9807904fcec057f831b9c0dd3ce73a6be9d0a2a
Eivind Uggedal <eivind@uggedal.com>
2016-02-16 12:22:38 +0000

    make: without guile

--- pkg/make.sh
+++ pkg/make.sh
@@ -2,7 +2,8 @@ ver 4.1
 dist $URI_GNU/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.bz2
 
 configure \
-	--disable-nls
+	--disable-nls \
+	--without-guile
 
 post_install() {
 	rm -r $MK_DESTDIR$MK_PREFIX/include
