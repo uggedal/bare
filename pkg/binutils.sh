6e4380ba830591520a3ee2d2fcf4ce1c7681905f
Eivind Uggedal <eivind@uggedal.com>
2016-02-20 18:23:48 +0000

    binutils: update to 2.26

--- pkg/binutils.sh
+++ pkg/binutils.sh
@@ -1,5 +1,7 @@
-ver 2.25.1
+ver 2.26
 dist $URI_GNU/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.bz2
+
+bdep bsdtar
 
 sub binutils-bld type bld
 sub binutils-bld rdep binutils
@@ -11,6 +13,10 @@ configure \
 	--build=$MK_BUILD_TRIPLE \
 	--host=$MK_HOST_TRIPLE \
 	--target=$MK_TARGET_TRIPLE
+
+if [ -z "$MK_CROSS" ] && [ -z "$MK_NO_DEP" ]; then
+	export TAR='bsdtar --no-same-owner'
+fi
 
 pre_configure() {
 	ed configure <<-EOF
