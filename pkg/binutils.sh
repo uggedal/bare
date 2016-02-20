fe4e8bbf537eb8ba05b3dee33e20872b7dee1ccd
Eivind Uggedal <eivind@uggedal.com>
2016-02-20 18:42:54 +0000

    binutils: use system libz

--- pkg/binutils.sh
+++ pkg/binutils.sh
@@ -1,22 +1,26 @@
 ver 2.26
 dist $URI_GNU/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.bz2
 
-bdep bsdtar
+bdep libz-bld bsdtar
 
 sub binutils-bld type bld
 sub binutils-bld rdep binutils
 
-configure \
+_configure="
 	--disable-multilib \
 	--disable-nls \
 	--disable-werror \
 	--build=$MK_BUILD_TRIPLE \
 	--host=$MK_HOST_TRIPLE \
 	--target=$MK_TARGET_TRIPLE
+	"
 
 if [ -z "$MK_CROSS" ] && [ -z "$MK_NO_DEP" ]; then
+	_configure="$_configure --with-system-zlib"
 	export TAR='bsdtar --no-same-owner'
 fi
+
+configure $_configure
 
 pre_configure() {
 	ed configure <<-EOF
