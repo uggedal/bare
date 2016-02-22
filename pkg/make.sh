e172ed6da7259d553cd19ad905f4650c63130fc7
Eivind Uggedal <eivind@uggedal.com>
2016-02-22 09:33:26 +0000

    make: needs bsdtar

--- pkg/make.sh
+++ pkg/make.sh
@@ -1,9 +1,15 @@
 ver 4.1
 dist $URI_GNU/$PKG_NAME/$PKG_NAME-${PKG_VER}.tar.bz2
 
+bdep bsdtar
+
 configure \
 	--disable-nls \
 	--without-guile
+
+if [ -z "$MK_CROSS" ] && [ -z "$MK_NO_DEP" ]; then
+	export TAR='bsdtar --no-same-owner'
+fi
 
 post_install() {
 	rm -r $MK_DESTDIR$MK_PREFIX/include
