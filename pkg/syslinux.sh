d40c393c7bdf8dca13647c0e8baffdc8e86dd8a1
Eivind Uggedal <eivind@uggedal.com>
2016-03-03 18:41:24 +0000

    syslinux: simplify

--- pkg/syslinux.sh
+++ pkg/syslinux.sh
@@ -4,13 +4,13 @@ dist https://www.kernel.org/pub/linux/utils/boot/$PKG_NAME/$PKG_NAME-$PKG_VER.ta
 bdep kernel-headers nasm perl
 
 sub syslinux-bios type custom
-sub syslinux-bios mv usr/lib/syslinux/'*'.c32
+sub syslinux-bios mv \
+	usr/lib/syslinux/'*'.c32 \
+	usr/lib/syslinux/*mbr.bin \
+	usr/lib/syslinux/memdisk
 
 sub syslinux-efi type custom
 sub syslinux-efi mv usr/lib/syslinux/efi64
-
-sub syslinux-mbr type custom
-sub syslinux-mbr mv usr/lib/syslinux/*mbr.bin
 
 sub extlinux type custom
 sub extlinux mv \
@@ -18,37 +18,11 @@ sub extlinux mv \
 	usr/share/man/man1/extlinux.1
 
 sub isolinux type custom
-sub isolinux mv \
-	usr/lib/syslinux/isolinux.bin \
+sub isolinux mv usr/lib/syslinux/isolinux.bin \
 	usr/lib/syslinux/isohd'*'x.bin
 
 sub pxelinux type custom
-sub pxelinux mv \
-	usr/lib/syslinux/isolinux.bin \
-	usr/lib/syslinux/'*'pxelinux.0
-
-sub syslinux-utils type custom
-sub syslinux-utils mv \
-	usr/bin/gethostip \
-	usr/bin/keytab-lilo \
-	usr/bin/lss16toppm \
-	usr/bin/md5pass \
-	usr/bin/memdiskfind \
-	usr/bin/mkdiskimage \
-	usr/bin/ppmtolss16 \
-	usr/bin/pxelinux-options \
-	usr/bin/sha1pass \
-	usr/bin/syslinux2ansi \
-	usr/share/man/man1/gethostip.1 \
-	usr/share/man/man1/keytab-lilo.1 \
-	usr/share/man/man1/lss16toppm.1 \
-	usr/share/man/man1/md5pass.1 \
-	usr/share/man/man1/memdiskfind.1 \
-	usr/share/man/man1/mkdiskimage.1 \
-	usr/share/man/man1/ppmtolss16.1 \
-	usr/share/man/man1/pxelinux-options.1 \
-	usr/share/man/man1/sha1pass.1 \
-	usr/share/man/man1/syslinux2ansi.1
+sub pxelinux mv usr/lib/syslinux/'*'pxelinux.0
 
 pre_configure() {
 	ed Makefile <<-"EOF"
